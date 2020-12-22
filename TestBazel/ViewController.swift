83f5b8e3275c4de93437eeef885270679cacac67
axl411 <axl411511@live.com>
2020-12-22 11:46:28 +0900

    init

---
+++ TestBazel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestBazel
+//
+//  Created by Gu Chao on 2020/12/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
